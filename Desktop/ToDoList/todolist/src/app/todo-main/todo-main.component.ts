30787c98217992b894efd42073f8fbe2cc991853
Achu-kappithan <achukappithan4236@gmail.com>
2025-03-09 23:34:36 +0530

    integrate list add remove functionality

--- Desktop/ToDoList/todolist/src/app/todo-main/todo-main.component.ts
+++ Desktop/ToDoList/todolist/src/app/todo-main/todo-main.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 interface Todo {
@@ -9,7 +16,7 @@ interface Todo {
 
 @Component({
   selector: 'app-todo-main',
-  imports: [],
+  imports: [MatButtonModule,MatFormFieldModule,MatIconModule,MatInputModule,MatCardModule,FormsModule,CommonModule],
   templateUrl: './todo-main.component.html',
   styleUrl: './todo-main.component.css'
 })
@@ -17,5 +24,24 @@ interface Todo {
 export class TodoMainComponent {
 
   list:Todo[] =[]
+  task:string ="" 
 
+
+  addtask() {
+    if (this.task.trim()) {
+      const newTask: Todo = {
+        id: Date.now(), 
+        title: this.task.trim(),
+        completed: false
+      };
+
+      this.list.push(newTask);
+      this.task = ''; 
+    }
+  }
+
+  remove(id:number){
+    console.log("working")
+    this.list = this.list.filter((val)=>val.id !=id)
+  }
 }
